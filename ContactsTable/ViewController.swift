3b647151320c5daf57a697cdfbbea8a89d8ad6b8
Alexandr <zhenuhin@inbox.ru>
2021-01-24 15:53:08 +0300

    Initial Commit

---
+++ ContactsTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsTable
+//
+//  Created by Александр Женухин on 24.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
